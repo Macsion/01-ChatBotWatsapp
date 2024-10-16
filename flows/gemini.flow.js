91ffb0f6d2a70b93d7041e4c095ae3abeeacd5dc
Macsion <maxi.colman24@gmail.com>
2024-10-16 14:13:47 -0300

    cree un try and catch en el flow de gemini para ver error

--- flows/gemini.flow.js
+++ flows/gemini.flow.js
@@ -6,26 +6,30 @@ const fs = require('fs');
 const pdf = require("pdf-parse");
 
 // Me conecto con drive para tener las credenciales y permisos de acceso
-const driveCredentials = JSON.parse(process.env.Drivechatbot);
+try {
+    const driveCredentials = JSON.parse(process.env.Drivechatbot);
 
-if (!driveCredentials) {
-    throw new Error('Drivechatbot environment variable is not defined');
-}
-// Ruta al archivo Drivechatbot.json
-const driveJsonPath = path.join(process.cwd(), 'Drivechatbot.json');
-
-// Verifica si el archivo Drivechatbot.json existe, si no, lo crea
-if (!fs.existsSync(driveJsonPath)) {
-    try {
-        fs.writeFileSync(driveJsonPath, JSON.stringify(driveCredentials, null, 2));
-        console.log('Drivechatbot.json file created successfully.');
-    } catch (error) {
-        throw new Error(`Failed to write Drivechatbot.json file: ${error.message}`);
+    if (!driveCredentials) {
+        throw new Error('Drivechatbot environment variable is not defined');
     }
-} else {
-    console.log('Drivechatbot.json file already exists.');
-}
+    // Ruta al archivo Drivechatbot.json
+    const driveJsonPath = path.join(process.cwd(), 'Drivechatbot.json');
 
+    // Verifica si el archivo Drivechatbot.json existe, si no, lo crea
+    if (!fs.existsSync(driveJsonPath)) {
+        try {
+            fs.writeFileSync(driveJsonPath, JSON.stringify(driveCredentials, null, 2));
+            console.log('Drivechatbot.json file created successfully.');
+        } catch (error) {
+            throw new Error(`Failed to write Drivechatbot.json file: ${error.message}`);
+        }
+    } else {
+        console.log('Drivechatbot.json file already exists.');
+    }
+}
+catch (error) {
+    console.error("Error parsing JSON:", response);
+}
 
 const { Storage } = require('@google-cloud/storage');
 const storage = new Storage({ keyFilename: './Drivechatbot.json' });
